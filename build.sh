69089ec8cb26e5d9db6d6d3dff06274b4f78eb5e
Bill Wang <bill.wang>
2021-01-07 18:50:51 +1100

    add build script

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Prerequisite
+# Make sure you set secret enviroment variables in Travis CI
+# DOCKER_USERNAME
+# DOCKER_PASSWORD
+# API_TOKEN
+
+# set -ex
+
+image="alpine/opencv"
+tag="latest"
+
+docker build -t ${image}:${tag}
+
+if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == false ]]; then
+    docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+    docker push ${image}:${tag}
+  fi
+}
